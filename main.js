9efd411fe23c069e3f694c81caf91d8515e02bd3
serignmbaye <barsaliou1998@gmail.com>
2023-05-04 19:34:47 +0000

    fix: empty file

--- main.js
+++ main.js
@@ -20,21 +20,23 @@ rl.on('line', (line) => {
 
 rl.on('close', () => {
     const nb = numbers.length;
-    let sumX = 0;
-    let sumY = 0;
-    let sumXY = 0;
-    let sumX2 = 0;
-    let sumY2 = 0;
-    for (let i = 0; i < nb; i++) {
-        sumX += i;
-        sumY += numbers[i];
-        sumXY += i * numbers[i];
-        sumX2 += i * i;
-        sumY2 += numbers[i] * numbers[i];
+    if (nb > 0) {
+        let sumX = 0;
+        let sumY = 0;
+        let sumXY = 0;
+        let sumX2 = 0;
+        let sumY2 = 0;
+        for (let i = 0; i < nb; i++) {
+            sumX += i;
+            sumY += numbers[i];
+            sumXY += i * numbers[i];
+            sumX2 += i * i;
+            sumY2 += numbers[i] * numbers[i];
+        }
+        const a = (nb * sumXY - sumX * sumY) / (nb * sumX2 - sumX * sumX);
+        const b = (sumY - a * sumX) / nb;
+        const r = (nb * sumXY - sumX * sumY) / Math.sqrt((nb * sumX2 - sumX * sumX) * (nb * sumY2 - sumY * sumY));
+        console.log(`Linear regression line: y = ${a.toFixed(6)}x + ${b.toFixed(6)}`);
+        console.log(`Pearson correlation coefficient: r = ${r.toFixed(10)}`);
     }
-    const a = (nb * sumXY - sumX * sumY) / (nb * sumX2 - sumX * sumX);
-    const b = (sumY - a * sumX) / nb;
-    const r = (nb * sumXY - sumX * sumY) / Math.sqrt((nb * sumX2 - sumX * sumX) * (nb * sumY2 - sumY * sumY));
-    console.log(`Linear regression line: y = ${a.toFixed(6)}x + ${b.toFixed(6)}`);
-    console.log(`Pearson correlation coefficient: r = ${r.toFixed(10)}`);
 });
